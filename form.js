f229b613f2dd2a029ff112ff3240dfebbd15e126
ShyngysM <tschingis.muratbek@gmail.com>
2022-05-18 22:24:50 +0200

    update

---
+++ form.js
@@ -0,0 +1,7 @@
+<script language="JavaScript">
+var frmvalidator  = new Validator("contactform");
+frmvalidator.addValidation("name","req","Please provide your name");
+frmvalidator.addValidation("email","req","Please provide your email");
+frmvalidator.addValidation("email","email",
+  "Please enter a valid email address");
+</script>
