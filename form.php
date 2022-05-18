a39b179a9cbae3f678a833a46997196427ca6023
ShyngysM <tschingis.muratbek@gmail.com>
2022-05-18 22:37:15 +0200

    update

--- form.php
+++ form.php
@@ -1,33 +1,11 @@
 <?php
-    $errors = '';
-    $myemail = 'tschingis.muratbek@gmail.com';//<-----Put Your email address here.
-    if(empty($_POST['name'])  ||
-       empty($_POST['email']) ||
-       empty($_POST['message']))
-    {
-        $errors .= "\n Error: all fields are required";
-    }
-    $name = $_POST['name'];
-    $email_address = $_POST['email'];
-    $message = $_POST['message'];
-    if (!preg_match(
-    "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
-    $email_address))
-    {
-        $errors .= "\n Error: Invalid email address";
-    }
-    
-    if( empty($errors))
-    {
-    $to = $myemail;
-    $email_subject = "Contact form submission: $name";
-    $email_body = "You have received a new message. ".
-    " Here are the details:\n Name: $name \n ".
-    "Email: $email_address\n Message \n $message";
-    $headers = "From: $myemail\n";
-    $headers .= "Reply-To: $email_address";
-    mail($to,$email_subject,$email_body,$headers);
-    //redirect to the 'thank you' page
-    header('Location: https://shyngysm.github.io/');
-    }
-    ?>
+
+if($_POST["message"]) {
+
+mail("tschingis.muratbek@gmail.com", "Here is the subject line",
+
+$_POST["insert your message here"]. "From: an@email.address");
+
+}
+
+?>
